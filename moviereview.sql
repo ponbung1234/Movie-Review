1ffd74de4a800282f11fabcff51ece19710c8bb5
Thanapon <31815206+ponbung1234@users.noreply.github.com>
2017-11-15 13:09:58 +0700

    new database

--- moviereview.sql
+++ moviereview.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 11, 2017 at 11:03 AM
--- Server version: 10.1.25-MariaDB
--- PHP Version: 7.1.7
+-- Generation Time: Nov 15, 2017 at 07:01 AM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -46,7 +46,8 @@ CREATE TABLE `movies` (
   `movieid` int(11) NOT NULL,
   `moviename` varchar(30) NOT NULL,
   `moviedetail` text NOT NULL,
-  `moviescore` int(11) NOT NULL,
+  `c_moviescore` int(11) NOT NULL,
+  `u_moviescore` int(11) NOT NULL,
   `moviepic` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -54,8 +55,9 @@ CREATE TABLE `movies` (
 -- Dumping data for table `movies`
 --
 
-INSERT INTO `movies` (`movieid`, `moviename`, `moviedetail`, `moviescore`, `moviepic`) VALUES
-(1, 'Godfather(1972)', '        <p> &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;When the aging head of a famous crime family decides to transfer his position to one of his subalterns, a series of unfortunate events start happening to the family, and a war begins between all the well-known families leading to insolence, deportation, murder and revenge, and ends with the favorable successor being finally chosen. Written by J. S. Golden</p>\r\n        <h3 class=\"my-3\">Details</h3>\r\n\r\n        <!-- Add link from IMDb -->\r\n\r\n        <h6>Official Sites:\r\n            <a href=\"http://www.paramount.com/movies/godfather-trilogy\" rel=\"nofollow\" itemprop=\"ur1\">GodFather Oficial Site</a>\r\n            \r\n        </h6>\r\n\r\n\r\n        <h6>Release Date:\r\n            <a >6 May 1972 (USA)</a>      \r\n        </h6>', 90, 'images/god1.jpg');
+INSERT INTO `movies` (`movieid`, `moviename`, `moviedetail`, `c_moviescore`, `u_moviescore`, `moviepic`) VALUES
+(1, 'Godfather(1972)', '        <p> &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;When the aging head of a famous crime family decides to transfer his position to one of his subalterns, a series of unfortunate events start happening to the family, and a war begins between all the well-known families leading to insolence, deportation, murder and revenge, and ends with the favorable successor being finally chosen. Written by J. S. Golden</p>\r\n        <h3 class=\"my-3\">Details</h3>\r\n\r\n        <!-- Add link from IMDb -->\r\n\r\n        <h6>Official Sites:\r\n            <a href=\"http://www.paramount.com/movies/godfather-trilogy\" rel=\"nofollow\" itemprop=\"ur1\">GodFather Oficial Site</a>\r\n            \r\n        </h6>\r\n\r\n\r\n        <h6>Release Date:\r\n            <a >6 May 1972 (USA)</a>      \r\n        </h6>', 8, 9, 'images/god1.jpg'),
+(2, 'IT', 'ss', 3, 4, 'images/it.jpg');
 
 -- --------------------------------------------------------
 
@@ -71,6 +73,14 @@ CREATE TABLE `user` (
   `userpicture` blob,
   `usertype` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `name`, `userpicture`, `usertype`) VALUES
+(1, 'ponbung1234', '123', 'thanpon', NULL, 'Top Critic'),
+(2, 'test1', '123', 'pon', NULL, 'User');
 
 -- --------------------------------------------------------
 
@@ -121,17 +131,17 @@ ALTER TABLE `userinfo`
 -- AUTO_INCREMENT for table `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `commentid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `commentid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 --
 -- AUTO_INCREMENT for table `movies`
 --
 ALTER TABLE `movies`
-  MODIFY `movieid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `movieid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT for table `userinfo`
 --
